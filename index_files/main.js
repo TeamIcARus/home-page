eca5b382fc7d6dfdbc4f86dc2bf23ab467feb3be
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2015-04-21 19:16:36 +0900

    github更新、英訳作業

---
+++ index_files/main.js
@@ -0,0 +1,40 @@
+$(function() {
+	$(window).resize(function() {
+		var _width = $(".view").css("width"); 
+		$(".view").css("height",_width);
+	});
+
+	$(window).ready(function() {
+		var _width = $(".view").css("width"); 
+		$(".view").css("height",_width);
+	});
+
+var linkTouchStart = function(){
+    thisAnchor = $(this);
+   touchPos = thisAnchor.offset().top;
+    moveCheck = function(){
+        nowPos = thisAnchor.offset().top;
+  	     if(touchPos == nowPos){
+            thisAnchor.addClass("hover");
+        }
+    }
+    setTimeout(moveCheck,100);
+}
+
+var linkTouchEnd = function(){
+    thisAnchor = $(this);
+    hoverRemove = function(){
+        thisAnchor.removeClass("hover");
+    }
+    setTimeout(hoverRemove,100);
+}
+ 
+
+
+
+
+$(document).on('touchstart mousedown','.view-first',linkTouchStart);
+$(document).on('touchend mouseup','.view-first',linkTouchEnd);
+
+
+});
